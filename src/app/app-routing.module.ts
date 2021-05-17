c1e4eaf2a56786c3ca5a673e5d425f726ba9677e
anas-ecam2020 <20316@ecam.be>
2021-05-17 17:25:15 +0200

    add components to menu bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { RecipeComponent } from './recipe/recipe.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path:'recipe',
+    component: RecipeComponent
+  }
 ];
 
 @NgModule({
