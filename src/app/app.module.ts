c182c98cc663750e82c337c4f95cd76e3244838b
anas-ecam2020 <20316@ecam.be>
2021-05-17 15:16:53 +0200

    Import created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { RecipeComponent } from './recipe/recipe.component';
+import { RecipeAddComponent } from './recipe-add/recipe-add.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, RecipeComponent, RecipeAddComponent, RecipeEditComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
